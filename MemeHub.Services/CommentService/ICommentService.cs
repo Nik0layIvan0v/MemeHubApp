f5f6415f8b689e0f7947f464878b8a0fb365dcec
Nikolay Ivanov <niki_8905@abv.bg>
2022-10-02 12:36:22 +0300

    Add comment and like service

---
+++ MemeHub.Services/CommentService/ICommentService.cs
@@ -0,0 +1,15 @@
+﻿namespace MemeHub.Services.CommentService
+{
+    public interface ICommentService
+    {
+        Task<int> CreateCommentAsync();
+
+        Task<int> EditCommentAsync();
+
+        Task<int> DeleteCommentAsync();
+
+        Task<int?> GetCommentCountAsync();
+
+        Task<int?> GetCommentAsync(int commentId);
+    }
+}
---
+++ MemeHub.Services/LikeService/ILikeService.cs
@@ -0,0 +1,23 @@
+﻿namespace MemeHub.Services.LikeService
+{
+    public interface ILikeService
+    {
+        Task<int> CreateLikeTypeAsync(string typeName, string userId);
+
+        Task<int> UpdateLikeTypeAsync(int targetLikeId, string typeName, string userId);
+
+        Task<bool> DeleteLikeTypeAsync(int targetLikeId);
+
+        Task<bool> AddLikeToMemeAsync(int memeId);
+
+        Task<bool> AddLikeToCommentAsync(int commentId);
+
+        Task<bool> AddLikeToUserAsync(string userId);
+
+        Task<bool> RemoveLikeFromMemeAsync(int memeId);
+
+        Task<bool> RemoveLikeFromCommentAsync(int commentId);
+
+        Task<bool> RemoveLikeFromUserAsync(int userId);
+    }
+}
