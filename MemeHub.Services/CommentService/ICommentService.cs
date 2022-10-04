c35170fae5f6aacb8b7d5cf8e4719c50039be706
Nikolay Ivanov <niki_8905@abv.bg>
2022-10-04 22:11:08 +0300

    Update comment service interface

--- MemeHub.Services/CommentService/ICommentService.cs
+++ MemeHub.Services/CommentService/ICommentService.cs
@@ -1,15 +1,25 @@
 ﻿namespace MemeHub.Services.CommentService
 {
+    using MemeHub.Database.Models;
+
     public interface ICommentService
     {
-        Task<int> CreateCommentAsync();
+        Task<int> CreateParrentCommentAsync(string userId, int memeId, string commentContent);
 
-        Task<int> EditCommentAsync();
+        Task<int> CreateChildCommentAsync(string userId, int memeId, string commentContent);
 
-        Task<int> DeleteCommentAsync();
+        Task<int> EditParrentCommentAsync(string userId, int memeId, int parrentCommentId, int childCommentId, string commentContent);
 
-        Task<int?> GetCommentCountAsync();
+        Task<int> EditChildCommentAsync(string userId, int memeId, int parrentCommentId, int childCommentId, string commentContent);
 
-        Task<int?> GetCommentAsync(int commentId);
+        Task<int> DeleteParrentCommentByIdAsync(int parrentCommentId);
+
+        Task<int> DeleteChildCommentByIdAsync(int chidlCommentId);
+
+        Task<int?> GetCommentsCountAsync(string userId, int memeId);
+
+        Task<Comment> GetCommentByIdAsync(int commentId);
+
+        Task<List<Comment>> GetAllMemeComments();
     }
 }
